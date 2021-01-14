88bf6e3d7c4b395c436fc18704b4d5296fe8c9f2
TheKOMX <thekom@hotmail.com>
2021-01-14 23:11:13 +0800

    reorganize structure for domain

--- app.py
+++ app.py
@@ -1,33 +1,9 @@
-import streamlit as st
-from src import home_page, chart_page
+from flask import Flask
+app = Flask(__name__)
 
-from src.markdown_module import get_markdown
-
-
-#--- Available Contents
-page_home = 'HOME'
-page_chart = 'Charts'
-page_morningstar = 'MorningStar Dashboard'
-page_about = 'About'
-pages = (page_home, page_chart, page_morningstar, page_about)
-
-def main():
-#--- Add Logo to Sidebar
-    st.sidebar.markdown(get_markdown('logo.md'), unsafe_allow_html=True)
-
-    #--- Add Menu to Sidebar
-    page_selected = st.sidebar.radio('Navigation', pages)
-    if page_selected == page_chart:
-        chart_page.get_chart()
-
-    elif page_selected==page_morningstar:
-        st.write('This is MorningStar Dashboard Page!')
-
-    elif page_selected==page_about:
-        st.write('This is About Page!')
-
-    else: #--- HOME Page
-        home_page.home()
+@app.route("/")
+def hello():
+    return "Welcome to The Macquarie Project!"
 
 if __name__ == "__main__":
-    main()
+    app.run()
